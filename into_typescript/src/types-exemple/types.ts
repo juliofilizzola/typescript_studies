4623fbd3863047a3d312574722bee5db8ed51674
Filizzola <juliofilizzola@hotmail.com>
2022-02-14 20:13:39 -0300

    :hammer::exemplos de array

--- into_typescript/src/types-exemple/types.ts
+++ into_typescript/src/types-exemple/types.ts
@@ -5,3 +5,10 @@ let adult: boolean = true;
 let mySymbol: symbol = Symbol('mySymbol')
 // let big: bigint = 10n; bigint não está disponivel no ecm2020
 
+// #Arrys
+
+let arrayNumber1: Array<number> = [1, 2, 3];
+let arrayNumber2: number[] = [1, 2, 3];
+let arrayString1: Array<string> = ['a', 'b'];
+let arrayString2: string[] = ['a', 'b'];
+
