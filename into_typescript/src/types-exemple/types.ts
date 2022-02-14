6870ae5f4590621c694a12bcd9c4d5a2ff8dc435
Filizzola <juliofilizzola@hotmail.com>
2022-02-14 16:00:28 -0300

    🎉: criando types

---
+++ into_typescript/src/types-exemple/types.ts
@@ -0,0 +1,7 @@
+/*  eslint-disable */
+let nome: string = 'xablau';
+let age: number = 30;
+let adult: boolean = true;
+let mySymbol: symbol = Symbol('mySymbol')
+// let big: bigint = 10n; bigint não está disponivel no ecm2020
+
