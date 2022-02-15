a5b3e81b35880bfde784627cc6bd49615e7bfd28
Filizzola <juliofilizzola@hotmail.com>
2022-02-15 12:48:22 -0300

    🎉: criando array

--- @types/src/types-exemple/type-array.ts
+++ @types/src/types-exemple/type-array.ts
@@ -1,4 +1,13 @@
 function mult(...args: Array<number>) {
   return args.reduce((acc, value) => acc * value, 1);
 }
-console.log(mult(1, 2));
+const five = 5;
+
+console.log(mult(five, 2), 'mult');
+
+function sub(...args: number[]) {
+  return args.reduce((acc, value) => acc - value, 1);
+}
+console.log(sub(1, 2), 'sub');
+
+// dois jeitos para declar um array.
