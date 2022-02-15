1097fa1eda0d85e99dade122bb4f6a44f507dfac
Filizzola <juliofilizzola@hotmail.com>
2022-02-15 09:19:52 -0300

    💥: criando objeto com função sem retorno

--- @types/src/types-exemple/type-void.ts
+++ @types/src/types-exemple/type-void.ts
@@ -3,3 +3,16 @@ function notReturn(...arg: string[]): void {
 }
 
 notReturn('top', 'isso aqui é demais');
+
+const person = {
+  name: 'Julio',
+  lastName: 'Filizzola',
+
+  concatName(): void {
+    console.log(this.name + ' ' + this.lastName);
+  },
+};
+
+person.concatName();
+
+export { person };
