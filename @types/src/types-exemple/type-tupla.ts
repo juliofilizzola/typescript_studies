590c470202da0431cb648c24ebd51b5932120d9c
Filizzola <juliofilizzola@hotmail.com>
2022-02-15 13:38:40 -0300

    🎉: tupla exemplo

---
+++ @types/src/types-exemple/type-tupla.ts
@@ -0,0 +1,8 @@
+const account1: [number, string] = [1, 'Julio'];
+
+console.log(account1);
+
+account1[0] = 100;
+
+console.log(account1);
+// um problema que temos aqui, é que podemos usar metodos como push e pop para modificar a tupla
