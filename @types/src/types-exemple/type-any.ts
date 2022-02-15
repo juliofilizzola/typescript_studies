1b470ff1cae25bcdf79b5fe96b8a2c567a90e6e6
Filizzola <juliofilizzola@hotmail.com>
2022-02-15 09:01:05 -0300

    💥: exemplo de type any (qualquer coisa)

---
+++ @types/src/types-exemple/type-any.ts
@@ -0,0 +1,9 @@
+function showMessage(msg: any): any { // => não apresente o tipo any, o codigo vai dar error.
+  return msg;
+}
+
+console.log(showMessage('xabalu'));
+console.log(showMessage({'xabalu': 'xabalu'}));
+console.log(showMessage(['xabalu']));
+console.log(showMessage(1));
+
