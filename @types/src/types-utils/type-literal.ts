9519d624a46ea5ccfc06f7a6e19efc6949eec2ee
Filizzola <juliofilizzola@hotmail.com>
2022-02-18 08:47:41 -0300

    🔨: tipo literal

---
+++ @types/src/types-utils/type-literal.ts
@@ -0,0 +1,8 @@
+let x = 10; // eslint-disable-line
+// o Typescritp inferiu a variavel x como number por isso podemos modificar ela para qualquer valor de numero, mas não podemos mudar para uma string, por exemplo
+
+x = 100;
+
+x = 'xablau';
+
+const y = 10; // o typeScript inferiu para um tipo literal (no caso o tipo 10)
