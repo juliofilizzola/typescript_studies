679ce047d82fb05418a42e15e678d114a0378311
Filizzola <juliofilizzola@hotmail.com>
2022-02-24 09:22:04 -0300

    ⚡️: class exempl

--- POO/src/class/class.ts
+++ POO/src/class/class.ts
@@ -1,11 +1,33 @@
 export class Empressa {
   public readonly name: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
 
-  constructor(name: string) {
+  constructor(name: string, cnpj: string) {
     this.name = name;
+    this.cnpj = cnpj;
+  }
+
+  adicionarColaboradores(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
   }
 }
 
-const empresa1 = new Empressa('Filizzola.end');
+export class Colaborador {
+  constructor(
+    public readonly name: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empressa('Filizzola.end', '1234545/32321');
 
 console.log(empresa1);
+
+console.log(empresa1.name);
