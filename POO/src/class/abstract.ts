ecdb9a681689bba4cd8e9a871c032456fd857435
Filizzola <juliofilizzola@hotmail.com>
2022-02-28 10:56:31 -0300

    ✨:exemplos

--- POO/src/class/abstract.ts
+++ POO/src/class/abstract.ts
@@ -12,4 +12,27 @@ export class Person {
   perderLife(powerAttack: number) {
     this.life - powerAttack;
   }
+
+  border() {
+    console.log('Bordão');
+  }
 }
+
+export class Guerreira extends Person {
+  border() {
+    console.log('pelos poderes de...');
+  }
+}
+
+export class Monster extends Person {
+  border() {
+    console.log('Morra Seiya!!!!!');
+  }
+}
+
+const lifePont = 1000;
+const powerAttackWirror = 122312;
+const powerAttackMonster = 12320;
+const monster = new Monster('xablau', powerAttackMonster, lifePont);
+const guerreira = new Guerreira('xoxana', powerAttackWirror, lifePont);
+guerreira.atack(monster);
