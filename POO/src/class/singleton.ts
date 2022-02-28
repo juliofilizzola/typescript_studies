d00292d1aa937ebdde6518e24688dc71af1ef0d3
Filizzola <juliofilizzola@hotmail.com>
2022-02-28 08:34:32 -0300

    ✨:finalziando exemplo de singleton

--- POO/src/class/singleton.ts
+++ POO/src/class/singleton.ts
@@ -1,5 +1,28 @@
+// export class Database {
+//   constructor(
+//     private host: string,
+//     private user: string,
+//     private password: string,
+//   ) {}
+
+//   connect(): void {
+//     console.log(`Conectando: ${this.host}, ${this.user}, ${this.password}`);
+//   }
+// }
+
+// const db1 = new Database('localhost', 'root', '12341');
+// db1.connect(); // primeira instancia do banco de dados.
+
+// const db2 = new Database('localhost', 'root', '12341');
+// db2.connect(); // mais uma instancia do banco de dados.
+
+// console.log(db1 === db2); // verificando se os dois são iguais.
+
+// metodo singleton - GOF
+
 export class Database {
-  constructor(
+  private static database: Database;
+  private constructor(
     private host: string,
     private user: string,
     private password: string,
@@ -8,12 +31,13 @@ export class Database {
   connect(): void {
     console.log(`Conectando: ${this.host}, ${this.user}, ${this.password}`);
   }
+
+  // Factorty Method - GoF
+  static getDatabase(host: string, user: string, password: string) {
+    if (Database.database) {
+      return Database.database;
+    }
+    Database.database = new Database(host, user, password);
+    return Database.database;
+  }
 }
-
-const db1 = new Database('localhost', 'root', '12341');
-db1.connect(); // primeira instancia do banco de dados.
-
-const db2 = new Database('localhost', 'root', '12341');
-db2.connect(); // mais uma instancia do banco de dados.
-
-console.log(db1 === db2); // verificando se os dois são iguais.
