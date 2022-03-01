c4b58f5aa7c066317cb62f02b77695d7aa25d625
Filizzola <juliofilizzola@hotmail.com>
2022-03-01 08:38:46 -0300

    ✨: final do exemplo de associação

--- POO/src/class/associacao.ts
+++ POO/src/class/associacao.ts
@@ -1,6 +1,26 @@
 export class Writer {
   private _tool: Tool | null = null;
   constructor(private _name: string) {}
+
+  set tool(tool: Tool | null) {
+    this._tool = tool;
+  }
+
+  get tool(): Tool | null {
+    return this._tool;
+  }
+
+  write(): void {
+    if (!this.tool) {
+      console.log('Não posso escrever sem ferramenta');
+      return;
+    }
+    this.tool.write();
+  }
+
+  get name(): string {
+    return this._name;
+  }
 }
 
 export abstract class Tool {
@@ -22,3 +42,14 @@ export class Typewriter extends Tool {
     console.log(`${this.name} está sendo utilizada`);
   }
 }
+
+const escritor = new Writer('Filizzola');
+console.log(escritor);
+
+const pen = new Pen('Bic');
+console.log(pen);
+
+const typewriter = new Typewriter('mela');
+console.log(typewriter);
+
+// as class's funcionam independente.
