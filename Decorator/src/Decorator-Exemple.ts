fb22333b669add685e97d848ced9b65fdcdf024a
Filizzola <juliofilizzola@hotmail.com>
2022-03-21 09:58:03 -0300

    ✨ : modificação da função dacorator

--- Decorator/src/Decorator-Exemple.ts
+++ Decorator/src/Decorator-Exemple.ts
@@ -2,11 +2,21 @@ export class Animal {
   constructor(public cor: string) {}
 }
 
-function decorator(target: any): any {
-  return target;
+function decorator<T extends new (...args: any[]) => any>(target: T) {
+  return class extends target {
+    cor: string;
+    constructor(...args: any[]) {
+      super(...args);
+      this.cor = 'qualquer coisa';
+    }
+  };
 }
 
 const AnimalDecorated = decorator(Animal);
 
 const animal = new AnimalDecorated('red');
+const animal2 = new AnimalDecorated('rexab');
+const animal3 = new AnimalDecorated('casa');
 console.log(animal);
+console.log(animal2);
+console.log(animal3);
