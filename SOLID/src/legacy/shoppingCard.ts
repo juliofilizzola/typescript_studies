873b9ec39b41f046d9119f109c047376a201f96a
Filizzola <juliofilizzola@hotmail.com>
2022-03-23 13:58:38 -0300

    ✨ : criando versão legada

---
+++ SOLID/src/legacy/shoppingCard.ts
@@ -0,0 +1,49 @@
+import { CartItem } from "../dto/cart-items";
+import { OrderStatus } from "../dto/ordem-status";
+
+export class ShoppingCartLegacy {
+  private readonly _items: CartItem[] = [];
+  private _orderStatus: OrderStatus = 'open';
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  total(): number {
+    return +this._items.reduce((total, next) => total + next.price, 0).toFixed(2);
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Carrinho vazio...');
+      return;
+    }
+    this._orderStatus = 'closed';
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean{
+    return this._items.length === 0;
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso!');
+  }
+
+  clear() {
+    this._items.length = 0;
+  }
+}
