302f73c6142d02ccd791862ad03237c9a244a660
Filizzola <juliofilizzola@hotmail.com>
2022-03-25 09:21:26 -0300

    ⚡️ : criando order e messaging

---
+++ SOLID/src/srp/class/entities/order.ts
@@ -0,0 +1,32 @@
+import { OrderStatus } from '../interfaces/order-status';
+import { Messaging } from '../../services/messaging';
+import { Persistency } from '../../services/persistency';
+import { ShoppingCart } from './ShoppingCart';
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open';
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly msg: Messaging,
+    private readonly saveOrder: Persistency,
+  ) {}
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Carrinho vazio...');
+      return;
+    }
+    this._orderStatus = 'closed';
+    this.msg.sendMessage(
+      `seu pedido com total de ${this.cart.total()} foi recebido`,
+    );
+
+    this.saveOrder.saveOrder();
+    this.cart.clear();
+  }
+}
---
+++ SOLID/src/srp/services/messaging.ts
@@ -0,0 +1,5 @@
+export class Messaging {
+  sendMessage(msg: string) {
+    console.log('Mensagem enviada: %s', msg);
+  }
+}
