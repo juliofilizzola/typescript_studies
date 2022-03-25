c111ae028bd1ca188807c2e74667262e8176a031
Filizzola <juliofilizzola@hotmail.com>
2022-03-25 13:32:35 -0300

    ⚡️ : class do exemplo isp

---
+++ SOLID/src/isp/class/Order.ts
@@ -0,0 +1,40 @@
+import { OrderStatus } from '../../dto/ordem-status';
+import { ShoppingCart } from './shoppingCart';
+import { Messaging } from '../services/messaging';
+import { Persistency } from '../services/persistency';
+import { CustomerOrder } from '../../dto/costomers-protocols';
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open';
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly messaging: Messaging,
+    private readonly persistency: Persistency,
+    private readonly customer: CustomerOrder,
+  ) {}
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.messaging.sendMessage(
+      `Seu pedido com total de ${this.cart.totalWithDicount()} foi recebido.`,
+    );
+    this.persistency.saveOrder();
+    this.cart.clear();
+
+    console.log(
+      'O cliente é:',
+      this.customer.getName(),
+      this.customer.getIDN(),
+    );
+  }
+}
