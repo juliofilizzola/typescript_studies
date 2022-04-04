286e56d73b16cf9144eef5ccc6e11063a0478458
Filizzola <juliofilizzola@hotmail.com>
2022-04-04 11:13:46 -0300

    ➕ : add dependencias do react

--- Project-Pomodoro/frontend-dev/.eslintrc.js
+++ Project-Pomodoro/frontend-dev/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:react/recommended',
   ],
   globals: {
     Atomics: 'readonly',
--- Project-Pomodoro/frontend-dev/src/App.tsx
+++ Project-Pomodoro/frontend-dev/src/App.tsx
@@ -1,21 +1,6 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(): JSX.Element {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return <div className="App">oi</div>;
 }
 
 export default App;
